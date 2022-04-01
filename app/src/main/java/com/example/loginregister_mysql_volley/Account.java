f368d94365a27c024eb8a905b1007cd7b337e199
StevenDjuarsa <78768786+StevenDjuarsa@users.noreply.github.com>
2022-04-02 00:12:54 +0700

    Update Account.java

--- app/src/main/java/com/example/loginregister_mysql_volley/Account.java
+++ app/src/main/java/com/example/loginregister_mysql_volley/Account.java
@@ -3,6 +3,7 @@ package com.example.loginregister_mysql_volley;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -12,8 +13,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -31,13 +35,41 @@ import java.util.Map;
 
 public class Account extends AppCompatActivity {
     Button dashboard;
+    ListView listview;
+
+    String[] action = {"Change Password", "Change Name", "Delete Account"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accountscreen);
 
-        dashboard = (Button) findViewById(R.id.dashboardFromAccount);
+        listview = (ListView) findViewById(R.id.list);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, action);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position==1){
+                    Intent chgPassIntent = new Intent(Account.this, changePass.class);
+                    startActivity(chgPassIntent);
+                }
+                else if (position==2){
+                    Intent chgNameIntent = new Intent(Account.this, changeName.class);
+                    startActivity(chgNameIntent);
+                }
+                else if (position==3){
+                    //delete account from DB
+                    Context context = getApplicationContext();
+                    Toast toast = Toast.makeText(context, "Account deleted", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
+
+        dashboard = (Button) findViewById(R.id.backToDashboard);
 
         dashboard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +78,10 @@ public class Account extends AppCompatActivity {
                 startActivity(dashboardIntent);
             }
         });
+
+
+
+
     }
 }
 
