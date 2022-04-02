f457239a94a6ee7ea6ee98ebc6bb328b69f0b5b1
Praggathi Raja Rao <praggathi.rr@gmail.com>
2022-04-01 18:13:46 -0700

    Update Settings.java

--- app/src/main/java/com/example/loginregister_mysql_volley/Settings.java
+++ app/src/main/java/com/example/loginregister_mysql_volley/Settings.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -31,6 +32,12 @@ import java.util.Map;
 
 public class Settings extends AppCompatActivity {
     Button dashboard;
+    TextView totalBudget;
+    TextView billsBudget;
+    TextView foodBudget;
+    TextView entertainmentBudget;
+    TextView otherBudget;
+    EditText edtTotalBudget, edtBillsBudget, edtFoodBudget, edtEntertainmentBudget, edtOtherBudget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +53,67 @@ public class Settings extends AppCompatActivity {
                 startActivity(dashboardIntent);
             }
         });
+
+        totalBudget = (TextView) findViewById(R.id.TotalBudget);
+        billsBudget = (TextView) findViewById(R.id.BillsBudget);
+        foodBudget = (TextView) findViewById(R.id.FoodBudget);
+        entertainmentBudget = (TextView) findViewById(R.id.EntertainmentBudget);
+        otherBudget = (TextView) findViewById(R.id.OtherBudget);
+
+        edtTotalBudget = (EditText) findViewById(R.id.editTotalBudget);
+        edtBillsBudget = (EditText) findViewById(R.id.editBillsBudget);
+        edtFoodBudget = (EditText) findViewById(R.id.editFoodBudget);
+        edtEntertainmentBudget = (EditText) findViewById(R.id.editEntertainmentBudget);
+        edtOtherBudget = (EditText) findViewById(R.id.editOtherBudget);
+
+        edtTotalBudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, "Total budget successfully changed", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        edtBillsBudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, "Bills budget successfully changed", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        edtFoodBudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, "Food budget successfully changed", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        edtEntertainmentBudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, "Entertainment budget successfully changed", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        edtOtherBudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, "Other budget successfully changed", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+
+
+
     }
 }
 
