ee3fa6dc269d89c62e7ddcbd664579e4bb6d9eb7
Haider Almandeel <haider.almandeel@sjsu.edu>
2022-04-07 23:43:17 -0700

    Changes to Dashboard
    Authors: Steven Djuarsa, Haider Almandeel, Aryan Maheshwari, Praggathi rajarao

--- app/src/main/java/com/example/loginregister_mysql_volley/Dashboard.java
+++ app/src/main/java/com/example/loginregister_mysql_volley/Dashboard.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,7 +17,7 @@ import androidx.navigation.ui.NavigationUI;
 
 public class Dashboard extends AppCompatActivity {
 
-    Button account, graphView, settings;
+    Button account, graphView, settings, addExpense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,39 @@ public class Dashboard extends AppCompatActivity {
         account = (Button) findViewById(R.id.account);
         graphView = (Button) findViewById(R.id.graphView);
         settings = (Button) findViewById(R.id.settings);
+        addExpense = (Button) findViewById(R.id.userFieldsAddExpense);
+
+        TextView budgetHint = (TextView) findViewById(R.id.userFieldsTotalBudget);
+        int totalBudget = 10000;
+        int totalBudgetSpent = 4000;
+        String budgetString = "Total Budget: " + totalBudgetSpent
+                + " out of " + totalBudget + " remaining";
+        budgetHint.setHint(budgetString);
+
+        TextView billsHint = (TextView) findViewById(R.id.userFieldsBillsBudget);
+        int bills = 2500;
+        int billsSpent = 1000;
+        String billsString = "Bills: " + billsSpent + " out of " + bills + " remaining";
+        billsHint.setHint(billsString);
+
+        TextView foodHint = (TextView) findViewById(R.id.userFieldsFoodBudget);
+        int food = 2500;
+        int foodSpent = 1000;
+        String foodString = "Food: " + foodSpent + " out of " + food + " remaining";
+        foodHint.setHint(foodString);
+
+        TextView entertainmentHint = (TextView) findViewById(R.id.userFieldsEntertainmentBudget);
+        int entertainment = 2500;
+        int entertainmentSpent = 1000;
+        String entertainmentString = "Entertainment: " + entertainmentSpent
+                + " out of " + entertainment + " remaining";
+        entertainmentHint.setHint(entertainmentString);
+
+        TextView othersHint = (TextView) findViewById(R.id.userFieldsOtherBudget);
+        int others = 2500;
+        int othersSpent = 1000;
+        String othersString = "Others: " + othersSpent + " out of " + others + " remaining";
+        othersHint.setHint(othersString);
 
 //        BottomNavigationView navView = findViewById(R.id.nav_view);
 //        // Passing each menu ID as a set of Ids because each
@@ -58,6 +93,14 @@ public class Dashboard extends AppCompatActivity {
             public void onClick(View v) {
                 Intent settingsIntent = new Intent(Dashboard.this, Settings.class);
                 startActivity(settingsIntent);
+            }
+        });
+
+        addExpense.setOnClickListener(new View.OnClickListener() {    //take to addExpense screen
+            @Override
+            public void onClick(View v) {
+                Intent addExpenseIntent = new Intent(Dashboard.this, addExpense.class);
+                startActivity(addExpenseIntent);
             }
         });
     }
