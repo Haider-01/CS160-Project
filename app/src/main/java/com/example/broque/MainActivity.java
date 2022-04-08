0f885fc02584ab331538d4b1cc5e8544b696972e
Crispino <madambacrispino@gmail.com>
2022-04-07 23:32:58 -0700

    Fix xml

--- app/src/main/java/com/example/broque/MainActivity.java
+++ app/src/main/java/com/example/broque/MainActivity.java
@@ -1,4 +1,15 @@
 package com.example.broque;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
 }
