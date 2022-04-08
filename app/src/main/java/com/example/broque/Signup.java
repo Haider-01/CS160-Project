a48e686f83a53cc87e72c2c4d5e1509a6799972e
danielpu23 <70337232+danielpu23@users.noreply.github.com>
2022-04-07 22:54:47 -0700

    Update Signup.java

--- app/src/main/java/com/example/broque/Signup.java
+++ app/src/main/java/com/example/broque/Signup.java
@@ -1,4 +1,24 @@
 package com.example.broque;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appconpat.app.AppConpatActivity;
+
+
 public class Signup {
+  Button buttonLogin, buttonRegister;
+  EditText editUsername, editEmail, editPassword, editConfPassword;
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+    buttonLogin = findViewByID(R.id.btn_login);
+    buttonRegister = findViewById(R.id.btn_register);
+    editUsername = findViewById(R.id.edt_usernameRegister);
+    editEmail = findViewById(R.id.edt_emailRegister);
+    editPassword = findViewByID(R.id.edt_passwordRegister);
+    editConfPassword = findViewById(R.id.edt_confPasswordRegister);
+  }
 }
