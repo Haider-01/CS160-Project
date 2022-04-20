2c8fe3f570a0c7266810f02339f711f56deb57c3
Crispino <madambacrispino@gmail.com>
2022-04-19 20:30:00 -0700

    Add asynctask button

--- app/src/main/java/com/example/broque/SignUp.java
+++ app/src/main/java/com/example/broque/SignUp.java
@@ -1,33 +1,22 @@
 package com.example.broque;
 
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.vishnusivadas.advanced_httpurlconnection.PutData;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
 
 
 public class SignUp extends AppCompatActivity{
@@ -46,87 +35,51 @@ public class SignUp extends AppCompatActivity{
         editUsername = findViewById(R.id.usernameSignup);
         editPassword = findViewById(R.id.passwordSignup);
         editPhone = findViewById(R.id.phonenumberSignup);
+    }//onCreate
 
-        buttonSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String fullname, username, password, phonenumber;
-                fullname = String.valueOf(editFullname.getText());
-                username = String.valueOf(editUsername.getText());
-                password = String.valueOf(editPassword.getText());
-                phonenumber = String.valueOf(editPhone.getText());
+    public void register(View view){
+        String fullname = editFullname.getText().toString();
+        String username = editUsername.getText().toString();
+        String password = editPassword.getText().toString();
+        String phonenumber = editPhone.getText().toString();
 
-                // Signup part
-                if (!fullname.equals("") && !username.equals("") && !password.equals("") && !phonenumber.equals("")) {
-                    Handler handler = new Handler(Looper.getMainLooper());
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                String link = "https://broke-test.herokuapp.com/signup.php?fullname="+fullname+"&username="+username+"&password="+password+"&phonenumber="+phonenumber;
-                                URL url = new URL(link);
-                                HttpClient client = new DefaultHttpClient();
-                                HttpGet request = new HttpGet();
-                                request.setURI(new URI(link));
-                                HttpResponse response = client.execute(request);
-                                BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        new RegisterButton().execute(fullname, username, password, phonenumber);
 
-                                StringBuffer sb = new StringBuffer("");
-                                String line = "";
+    }//register
 
-                                while ((line = in.readLine()) != null) {
-                                    sb.append(line);
-                                    break;
-                                }
+    private class RegisterButton extends AsyncTask<String, Void, Void> {
+        @Override
+        protected Void doInBackground(String... arg) {
+            try{
+                String fullname = (String)arg[0];
+                String username = (String)arg[1];
+                String password = (String)arg[2];
+                String phonenumber = (String)arg[3];
+                String link = "https://broke-test.herokuapp.com/signup.php?fullname="+fullname+"&username="+username+"&password="+password+"&phonenumber="+phonenumber;
+                URL url = new URL(link);
+                HttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                request.setURI(new URI(link));
+                HttpResponse response = client.execute(request);
+                BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                StringBuffer sb = new StringBuffer("");
+                String line = "";
 
-                                in.close();
-                                Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_SHORT).show();
-                                System.out.println("account created");
-//                                String link="https://broke-test.herokuapp.com/signup.php";
-//                                String data  = URLEncoder.encode("username", "UTF-8") + "=" +
-//                                        URLEncoder.encode(fullname, "UTF-8");
-//                                data += "&" + URLEncoder.encode("username", "UTF-8") + "=" +
-//                                        URLEncoder.encode(username, "UTF-8");
-//                                data += "&" + URLEncoder.encode("password", "UTF-8") + "=" +
-//                                        URLEncoder.encode(password, "UTF-8");
-//                                data += "&" + URLEncoder.encode("phonenumber", "UTF-8") + "=" +
-//                                        URLEncoder.encode(phonenumber, "UTF-8");
-//
-//                                URL url = new URL(link);
-//                                URLConnection conn = url.openConnection();
-//
-//                                conn.setDoOutput(true);
-//                                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-//
-//                                wr.write( data );
-//                                wr.flush();
-//                                URL url = new URL(link);
-//                                HttpURLConnection httpConn = (HttpURLConnection)url.openConnection();
-//                                httpConn.setRequestMethod("GET");
-//                                try {
-//                                    InputStream in = new BufferedInputStream(httpConn.getInputStream());
-//                                    readStream(in);
-//                                } finally {
-//                                    httpConn.disconnect();
-//                                }
-//                                HttpClient client = new DefaultHttpClient();
-//                                HttpGet request = new HttpGet();
-//                                request.setURI(new URI(link));
-//                                HttpResponse response = client.execute(request);
-                            } catch (Exception e) {
-                                System.out.println(e);
-                                System.out.println("error!");
-                            }
-                        }
-                    });
-                } else {
-                    Toast.makeText(getApplicationContext(), "All fields required", Toast.LENGTH_SHORT).show();
-                    System.out.println("account not created");
+                while ((line = in.readLine()) != null) {
+                    sb.append(line);
+                    break;
                 }
-            }
-        });
 
-    }
+                in.close();
+                Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_SHORT).show();
+            } catch(Exception e){
+                System.out.println(e);            }
+            return null;
+        }//doInBackground
+    }//RegisterButton
+
 
 
 }
+
+
