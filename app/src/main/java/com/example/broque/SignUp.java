cf38f05d985ca4f7e28c3c46c11628b3702c059a
Crispino Madamba <madambacrispino@gmail.com>
2022-04-21 02:05:35 -0700

    Almost fix url connection

--- app/src/main/java/com/example/broque/SignUp.java
+++ app/src/main/java/com/example/broque/SignUp.java
@@ -28,34 +28,54 @@ public class SignUp extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        buttonLogin = findViewById(R.id.btn_logSignup);
-        buttonSignup = findViewById(R.id.btn_createSignup);
+//        buttonLogin = findViewById(R.id.btn_logSignup);
+//        buttonSignup = findViewById(R.id.btn_createSignup);
 
         editFullname = findViewById(R.id.fullnameSignup);
         editUsername = findViewById(R.id.usernameSignup);
         editPassword = findViewById(R.id.passwordSignup);
         editPhone = findViewById(R.id.phonenumberSignup);
+
+        // for asynctask
+        buttonSignup = (Button) findViewById(R.id.btn_createSignup);
+        this.buttonSignup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                // do the work here
+                System.out.println("onCreate listener invoked");
+                String fullname = editFullname.getText().toString();
+                String username = editUsername.getText().toString();
+                String password = editPassword.getText().toString();
+                String phonenumber = editPhone.getText().toString();
+                new RegisterButton().execute(fullname, username, password, phonenumber);
+            }// onCLick
+        });// setOnClickListener
     }//onCreate
 
     public void register(View view){
-        String fullname = editFullname.getText().toString();
-        String username = editUsername.getText().toString();
-        String password = editPassword.getText().toString();
-        String phonenumber = editPhone.getText().toString();
-
-        new RegisterButton().execute(fullname, username, password, phonenumber);
+//        System.out.println("register method invoked");
+//        String fullname = editFullname.getText().toString();
+//        String username = editUsername.getText().toString();
+//        String password = editPassword.getText().toString();
+//        String phonenumber = editPhone.getText().toString();
+//
+//        new RegisterButton().execute(fullname, username, password, phonenumber);
+//        System.out.println("executing registerbutton");
 
     }//register
+
 
     private class RegisterButton extends AsyncTask<String, Void, Void> {
         @Override
         protected Void doInBackground(String... arg) {
             try{
+                System.out.println("inside try block");
                 String fullname = (String)arg[0];
+                System.out.println(fullname);
                 String username = (String)arg[1];
                 String password = (String)arg[2];
                 String phonenumber = (String)arg[3];
                 String link = "https://broke-test.herokuapp.com/signup.php?fullname="+fullname+"&username="+username+"&password="+password+"&phonenumber="+phonenumber;
+                System.out.println(link);
                 URL url = new URL(link);
                 HttpClient client = new DefaultHttpClient();
                 HttpGet request = new HttpGet();
@@ -71,14 +91,16 @@ public class SignUp extends AppCompatActivity{
                 }
 
                 in.close();
-                Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_SHORT).show();
+                System.out.println("account created");
             } catch(Exception e){
-                System.out.println(e);            }
+                System.out.println(e);
+                System.out.println("account not created");
+            }
             return null;
         }//doInBackground
+
     }//RegisterButton
-
-
 
 }
 
