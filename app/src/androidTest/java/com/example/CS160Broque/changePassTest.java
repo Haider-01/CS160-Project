ff79c9afc0f180c41801c0edf2cb7dc430d8b029
Haider Almandeel <haider.almandeel@sjsu.edu>
2022-04-21 22:43:24 -0700

    Included imports for changePassTest

--- app/src/androidTest/java/com/example/CS160Broque/changePassTest.java
+++ app/src/androidTest/java/com/example/CS160Broque/changePassTest.java
@@ -8,8 +8,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
