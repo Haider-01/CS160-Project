6efbfd94bb42d1256900948ee56cdbadb8812cc2
aaronli-git <li.aaron36@gmail.com>
2022-04-22 00:40:43 -0700

    Implemented Dashboard addExpense button with Async

--- app/src/main/java/com/example/broque/Dashboard.java
+++ app/src/main/java/com/example/broque/Dashboard.java
@@ -1,67 +1,56 @@
-//package com.example.loginregister_mysql_volley;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.TextView;
-//
-//import com.google.android.material.bottomnavigation.BottomNavigationView;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.navigation.NavController;
-//import androidx.navigation.Navigation;
-//import androidx.navigation.ui.AppBarConfiguration;
-//import androidx.navigation.ui.NavigationUI;
-//
-//public class Dashboard extends AppCompatActivity {
-//
-//    Button account, graphView, settings, addExpense;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_dashboard);
-//
-//        account = (Button) findViewById(R.id.account);
-//        graphView = (Button) findViewById(R.id.graphView);
-//        settings = (Button) findViewById(R.id.settings);
-//        addExpense = (Button) findViewById(R.id.userFieldsAddExpense);
-//
-//        TextView budgetHint = (TextView) findViewById(R.id.userFieldsTotalBudget);
-//        int totalBudget = 10000;
-//        int totalBudgetSpent = 4000;
-//        String budgetString = "Total Budget: " + totalBudgetSpent
-//                + " out of " + totalBudget + " remaining";
-//        budgetHint.setHint(budgetString);
-//
-//        TextView billsHint = (TextView) findViewById(R.id.userFieldsBillsBudget);
-//        int bills = 2500;
-//        int billsSpent = 1000;
-//        String billsString = "Bills: " + billsSpent + " out of " + bills + " remaining";
-//        billsHint.setHint(billsString);
-//
-//        TextView foodHint = (TextView) findViewById(R.id.userFieldsFoodBudget);
-//        int food = 2500;
-//        int foodSpent = 1000;
-//        String foodString = "Food: " + foodSpent + " out of " + food + " remaining";
-//        foodHint.setHint(foodString);
-//
-//        TextView entertainmentHint = (TextView) findViewById(R.id.userFieldsEntertainmentBudget);
-//        int entertainment = 2500;
-//        int entertainmentSpent = 1000;
-//        String entertainmentString = "Entertainment: " + entertainmentSpent
-//                + " out of " + entertainment + " remaining";
-//        entertainmentHint.setHint(entertainmentString);
-//
-//        TextView othersHint = (TextView) findViewById(R.id.userFieldsOtherBudget);
-//        int others = 2500;
-//        int othersSpent = 1000;
-//        String othersString = "Others: " + othersSpent + " out of " + others + " remaining";
-//        othersHint.setHint(othersString);
-//
-//
+package com.example.broque;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.broque.R;
+import com.example.broque.SignUp;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+
+public class Dashboard extends AppCompatActivity {
+
+    Button account, graphView, settings, addExpense;
+    EditText editTotal, editBill, editFood, editEntertainment, editOther;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        account = (Button) findViewById(R.id.account);
+        graphView = (Button) findViewById(R.id.graphView);
+        settings = (Button) findViewById(R.id.settings);
+        addExpense = (Button) findViewById(R.id.userFieldsAddExpense);
+
+        editTotal = findViewById(R.id.userFieldsTotalBudget);
+        editBill = findViewById(R.id.userFieldsBillsBudget);
+        editFood = findViewById(R.id.userFieldsFoodBudget);
+        editEntertainment = findViewById(R.id.userFieldsEntertainmentBudget);
+        editOther = findViewById(R.id.userFieldsOtherBudget);
+
+
+
+
 //        account.setOnClickListener(new View.OnClickListener() { //take to account screen
 //            @Override
 //            public void onClick(View v) {
@@ -85,14 +74,58 @@
 //                startActivity(settingsIntent);
 //            }
 //        });
-//
-//        addExpense.setOnClickListener(new View.OnClickListener() {    //take to addExpense screen
-//            @Override
-//            public void onClick(View v) {
-//                Intent addExpenseIntent = new Intent(Dashboard.this, addExpense.class);
-//                startActivity(addExpenseIntent);
-//            }
-//        });
-//    }
-//
-//}
+
+        // for asynctask
+        addExpense.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                // do the work here
+                System.out.println("onCreate listener invoked");
+                String total = editTotal.getText().toString();
+                String bill = editBill.getText().toString();
+                String food = editFood.getText().toString();
+                String entertainment = editEntertainment.getText().toString();
+                String other = editOther.getText().toString();
+                new Dashboard.addExpenseButton().execute(total, bill, food, entertainment, other);
+            }// onCLick
+        });// setOnClickListener
+    }// on create
+
+    private class addExpenseButton extends AsyncTask<String, Void, Void> {
+        @Override
+        protected Void doInBackground(String... arg) {
+            try{
+                // check inputs
+                String total = (String)arg[0];
+                String bill = (String)arg[1];
+                String food = (String)arg[2];
+                String entertainment = (String)arg[3];
+                String other = (String)arg[4];
+                String username = "tim";
+                String link = "https://broke-test.herokuapp.com/addExpense.php?total=%22"+total+"%22&bill=%22"+bill+"%22&food=%22"+food+"%22&entertainment=%22"+entertainment+"%22&other=%22"+other+"%22&username=%22"+username+"%22";
+                System.out.println(link);
+                URL url = new URL(link);
+                HttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                request.setURI(new URI(link));
+                HttpResponse response = client.execute(request);
+                BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                StringBuffer sb = new StringBuffer("");
+                String line = "";
+
+                while ((line = in.readLine()) != null) {
+                    sb.append(line);
+                    System.out.println("From sb" + sb);
+                    break;
+                }
+                in.close();
+                System.out.println("Expenses added");
+            } catch(Exception e){
+                System.out.println(e);
+                System.out.println("Expenses not added");
+            }
+            return null;
+        }//doInBackground
+
+    }//RegisterButton
+
+}
