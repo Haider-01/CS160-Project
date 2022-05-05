7759eec0c5f434f122fcebdb175e1a33bfcc21a3
Steven Djuarsa <steven.djuarsa@sjsu.edu>
2022-05-06 06:03:36 +0700

    Pie Chart

--- app/src/main/java/com/example/CS160Broque/Dashboard.java
+++ app/src/main/java/com/example/CS160Broque/Dashboard.java
@@ -1,6 +1,7 @@
 package com.example.CS160Broque;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,10 +9,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.gson.Gson;
 
 public class Dashboard extends AppCompatActivity {
@@ -19,6 +28,7 @@ public class Dashboard extends AppCompatActivity {
     Button btn_account, graphView, settings, addExpense;
     String jsonMyAccount;
     Account account;
+    PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +39,9 @@ public class Dashboard extends AppCompatActivity {
         graphView = (Button) findViewById(R.id.btn_graph_dashboard);
         settings = (Button) findViewById(R.id.btn_settings_dashboard);
         addExpense = (Button) findViewById(R.id.btn_addExpense_dashboard);
+        pieChart = (PieChart) findViewById(R.id.dashboard_piechart);
+        setupPieChart();
+        loadPieChartData();
 
         // Get from bundle
         Bundle extras = getIntent().getExtras();
@@ -118,6 +131,58 @@ public class Dashboard extends AppCompatActivity {
                 startActivity(goToAddExpense);
             }
         });
+    }
+
+    private void setupPieChart() {
+        pieChart.setDrawHoleEnabled(true);
+//        pieChart.setUsePercentValues(true);
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setCenterText("Spending by Category");
+        pieChart.setCenterTextSize(24);
+        pieChart.getDescription().setEnabled(false);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setEnabled(true);
+    }
+
+    private void loadPieChartData(){
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(20, "Food & Dining"));
+        entries.add(new PieEntry(15, "Medical"));
+        entries.add(new PieEntry(10, "Entertainment"));
+        entries.add(new PieEntry(25, "Electricity and Gas"));
+        entries.add(new PieEntry(30, "Housing"));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int color: ColorTemplate.MATERIAL_COLORS) {
+            colors.add(color);
+        }
+
+        for (int color : ColorTemplate.VORDIPLOM_COLORS){
+            colors.add(color);
+        }
+
+
+
+        PieDataSet dataSet = new PieDataSet(entries, "Expense Category");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        //data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+
+        pieChart.setData(data);
+        pieChart.invalidate();
+
+        pieChart.animateY(1400, Easing.EaseInOutQuad);
+
     }
 
 }
